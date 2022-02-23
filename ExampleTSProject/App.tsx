64704b981f67873703d56aaf7951c205135029a1
Glenn Harper <glharper@microsoft.com>
2022-02-23 09:21:40 -0800

    fix for "crypto.getRandomValues not supported" error

--- ExampleTSProject/App.tsx
+++ ExampleTSProject/App.tsx
@@ -24,6 +24,7 @@
  import {
    Colors,
  } from 'react-native/Libraries/NewAppScreen';
+ import 'react-native-get-random-values';
  import { AudioConfig, CancellationDetails, CancellationReason, NoMatchDetails, NoMatchReason, ResultReason, SpeechConfig, SpeechRecognizer } from 'microsoft-cognitiveservices-speech-sdk';
  import DocumentPicker from 'react-native-document-picker';
  import RNFS from 'react-native-fs';
